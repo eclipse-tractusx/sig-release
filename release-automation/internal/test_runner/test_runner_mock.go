bc958a8e6c53d87aa4261db29d1a57bdb9ebd495
Sebastian Bezold <sebastian.bezold@mercedes-benz.com>
2023-11-03 08:55:25 +0100

    refactor: integrate test runner and local check command

--- release-automation/internal/test_runner/test_runner_mock.go
+++ release-automation/internal/test_runner/test_runner_mock.go
@@ -17,7 +17,7 @@
  * SPDX-License-Identifier: Apache-2.0
  ******************************************************************************/
 
-package txqualitychecks
+package testrunner
 
 import (
 	"fmt"
