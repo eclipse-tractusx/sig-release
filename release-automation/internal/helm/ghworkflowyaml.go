b735e6b4c2946f123dc798d07b8a945bbbd318b8
Tomasz Barwicki <tomasz.barwicki@t-systems.com>
2023-12-11 14:37:29 +0100

    chore: add github workflow model

---
+++ release-automation/internal/helm/ghworkflowyaml.go
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
+
+package helm
+
+
+type GitHubWorkflow struct {
+	Name string `yaml:"name"`
+	On map[string]interface{} `yaml:"on"`
+	Jobs map[string]Job 
+}
+
+type Job struct {
+	RunsOn string `yaml:"runs-on"`
+	Steps	[]Step
+}
+
+type Step struct {
+	Name string `yaml:"name"`
+	Uses string `yaml:"uses"`
+	Run string `yaml:"runs"`
+}
+
+type Trigger struct {
+	Events []string
+}
