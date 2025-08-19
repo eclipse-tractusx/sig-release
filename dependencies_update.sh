5b5fca99899cd6249ff96849639fbbb1c8ba4065
Tom <tom.meyer@isst.fraunhofer.de>
2025-08-19 13:10:22 +0200

    chore(dependencies_update.sh): add missing license header

--- dependencies_update.sh
+++ dependencies_update.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 #
+# Copyright (c) 2023 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License, Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+#
 # Script used to generate concatenated dependencies from TRG dashboard & release notifier.
 # Ensure you have latest Eclipse Dash License tool:
 # https://repo.eclipse.org/service/local/artifact/maven/redirect?r=dash-licenses&g=org.eclipse.dash&a=org.eclipse.dash.licenses&v=LATEST
